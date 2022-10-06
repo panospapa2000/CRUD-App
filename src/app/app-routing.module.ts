443b360d65491a626854057920fdf11c1730af6d
Panagiotis <panagiotis.papaioannou@onelity.com>
2022-10-06 10:42:32 +0300

    button creation+routlinking

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { createComponent, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { HomeComponent } from './home/home.component';
+import { UpdateComponent } from './update/update.component';
+import { ViewComponent } from './view/view.component';
+
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "create", component: CreateComponent },
+  {
+    path: "view", component: ViewComponent,
+    children: [
+      {path:"update", component: UpdateComponent},
+      {path:"delete", component: UpdateComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ViewComponent } from './view/view.component';
+import { CreateComponent } from './create/create.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ViewComponent,
+    CreateComponent,
+    UpdateComponent,
+    DeleteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
